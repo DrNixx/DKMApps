acd743a630f82b7cd211f7a0b87be8010a56858b
Nick Panteleeff <drnix@hotmail.com>
2022-01-31 23:50:55 +0300

    Work

--- DKMObserver/ObservePanel.Designer.cs
+++ DKMObserver/ObservePanel.Designer.cs
@@ -194,6 +194,8 @@ namespace DKMObserver
             // 
             // lbPatient
             // 
+            this.lbPatient.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.lbPatient.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.lbPatient.Location = new System.Drawing.Point(7, 204);
             this.lbPatient.Name = "lbPatient";
